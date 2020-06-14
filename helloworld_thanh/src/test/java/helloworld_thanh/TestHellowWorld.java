a1c622dde89a8047157e1e74dd3d60f7328b1e3f
Thanh <thanhphamnl@gmail.com>
2020-06-13 21:25:48 -0700

    Commit int

---
+++ helloworld_thanh/src/test/java/helloworld_thanh/TestHellowWorld.java
@@ -0,0 +1,12 @@
+package helloworld_thanh;
+
+import org.testng.annotations.Test;
+
+public class TestHellowWorld {
+	
+	@Test
+	public void testHellowWorld() {
+		System.out.println(" Hello World!");
+	}
+
+}
